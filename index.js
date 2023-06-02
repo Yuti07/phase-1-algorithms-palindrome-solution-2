bd2ee793b7856d2de3383de8b15c46d865b3c420
Yuti Thakkar <thakkaryuti@gmail.com>
2023-06-02 12:34:06 -0400

    adds algo additional

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-function isPalindrome(word) {
+
   // Write your algorithm here
+
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
 
-/* 
+
+/*
   Add your pseudocode here
 */
+// iterate from the beginning of the string to the middle of the string
+//   compare the letter we're iterating over to the corresponding letter at the end of the string
+//     if the letters don't match, return false
 
+// if we reach the middle, and all the letters match, return true
 /*
   Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
   // add your own custom tests in here
+function isPalindrome(word) {
+  // Compare the word with its reverse
+  return word === word.split('').reverse().join('');
+}
+console.log(isPalindrome('abba'));      // true
+console.log(isPalindrome('racecar'));   // true
+console.log(isPalindrome('a'));         // true
+console.log(isPalindrome('robot'));     // false
+console.log(isPalindrome('ab'));        // false
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +37,5 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
 
 module.exports = isPalindrome;
